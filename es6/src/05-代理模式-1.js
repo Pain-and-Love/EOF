e102397bac8e74a7f0e06ecadcf6d98f0930f834
XiaFan <xiafan@corp.netease.com>
2018-12-13 16:36:33 +0800

    【update】proxy 代理模式

--- es6/src/05-代理模式-1.js
+++ es6/src/05-代理模式-1.js
@@ -7,8 +7,10 @@ let star = {
 
 // 经纪人
 let agent = new Proxy(star, {
-    get: function (target, key) {
+    get: function (target, key, receiver) {
+        // receiver就是 经纪人
         if (key === 'phone') {
+            console.log('target ->', target);
             // 返回经纪人自己的手机号
             return '18611112222'
         }
@@ -16,9 +18,10 @@ let agent = new Proxy(star, {
             // 明星不报价，经纪人报价
             return 120000
         }
+        // 返回正常的值
         return target[key]
     },
-    set: function (target, key, val) {
+    set: function (target, key, val, receiver) {
         if (key === 'customPrice') {
             if (val < 100000) {
                 // 最低 10w
@@ -41,3 +44,25 @@ console.log(agent.price)
 agent.customPrice = 150000
 // agent.customPrice = 90000  // 报错：价格太低
 console.log('customPrice', agent.customPrice)
+
+
+// https://www.2cto.com/kf/201707/654176.html
+var foo = {};
+var bar = new Proxy(foo, {
+    get: function (target, key, receiver) {
+        console.log("This is get! The key: " + key);
+        console.log(receiver === bar, foo === target);
+        return target[key];
+    }
+});
+bar.a = 1;
+console.log(bar.a);
+//This is get! The key: a
+//true true
+//1
+ 
+bar.b = {c: "d"};
+console.log(bar.b.c);
+//This is get! The key: b
+//true true
+//"d"
